42ab927ed771014c12d1bb1b8cc687cd617974b5
Becca37 <50496403+Becca37@users.noreply.github.com>
2019-05-28 13:09:13 -0600

    Update map.js
    Resolves issues #34

--- src/map.js
+++ src/map.js
@@ -691,27 +691,23 @@ function watchLocation(map)
 						{
 							lat: position.coords.latitude,
 							lng: position.coords.longitude
-						};
-						
-						var icon = 
-						{
-							path: "M27.648-41.399q0-3.816-2.7-6.516t-6.516-2.7-6.516 2.7-2.7 6.516 2.7 6.516 6.516 2.7 6.516-2.7 2.7-6.516zm9.216 0q0 3.924-1.188 6.444l-13.104 27.864q-.576 1.188-1.71 1.872t-2.43.684-2.43-.684-1.674-1.872l-13.14-27.864q-1.188-2.52-1.188-6.444 0-7.632 5.4-13.032t13.032-5.4 13.032 5.4 5.4 13.032z",
-							fillColor: '#E32831',
-							fillOpacity: .5,
-							strokeWeight: 0,
-							scale: 0.65
-						}		
+						};	
+
+						var currentLocationSvg = document.getElementById('svgIcon').innerHTML;
 						
 						if(!currentLocationMarker)
 						{
 							currentLocationMarker = new google.maps.Marker
 							(
 								{
-									id: 'myCurrentPosition',
 									position: pos,
 									map: map,
 									title:  'Approximate Current Location',
-									icon: icon
+									icon: 
+									{ 
+										  url: 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(currentLocationSvg)
+										, scaledSize: new google.maps.Size(50, 50)
+									}
 								}
 							);
 						}
@@ -796,6 +792,10 @@ function goToCurrentLocation()
 			var currentPosition = currentLocationMarker.position;
 			map.zoom = 16;
 			map.panTo(currentPosition);
+			if (!currentLocationMarker.animating)
+			{
+				currentLocationMarker.setAnimation(4);
+			}
 		}
 		else
 		{		
